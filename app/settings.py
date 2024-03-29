7eaf231e8c76a3c92e8323da8abdbd9335fbba11
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-29 18:31:36 -0300

    feat: implementando compradores

--- app/settings.py
+++ app/settings.py
@@ -141,9 +141,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "PAGE_SIZE": 10,
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ]
 }
 
 SPECTACULAR_SETTINGS = {
