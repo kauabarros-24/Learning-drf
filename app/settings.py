26b6f282c69f32614931507a45c9469e1f19d411
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-04 10:32:49 -0300

    caminhos para acessar as imagens

--- app/settings.py
+++ app/settings.py
@@ -152,5 +152,8 @@ SPECTACULAR_SETTINGS = {
 
 print(f"MODE: {MODE} \nMEDIA_URL: {MEDIA_URL} \nDATABASE: {DATABASES}")
 
-#Acessar imagens
-
+#Acessar imagens:
+MEDIA_URL = "http://0.0.0.0:19003/api/media/"
+MEDIA_ENDPOINT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
+FILE_UPLOAD_PERMISSIONS = 0o640
